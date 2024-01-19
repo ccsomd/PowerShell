bd8f44f839cc533f891d0bc5742bf8a35fadcce3
Lehi <17449+lehisanchez@users.noreply.github.com>
2024-01-19 12:10:31 -0500

    Updated scripts

--- 365/Exchange/rooms.ps1
+++ 365/Exchange/rooms.ps1
@@ -1,5 +1,5 @@
 # Authenticate with 365
-Connect-ExchangeOnline -UserPrincipalName #<username>
+Connect-ExchangeOnline -UserPrincipalName <Username>
 
 # List all RoomLists
 Get-DistributionGroup -ResultSize Unlimited | Where {$_.RecipientTypeDetails -eq "RoomList"} | Format-Table DisplayName,Identity,PrimarySmtpAddress –AutoSize
@@ -9,10 +9,10 @@ Get-Mailbox -ResultSize Unlimited -Filter {RecipientTypeDetails -eq "RoomMailbox
 
 
 # New Room List
-New-Distributiongroup -Name 'Room List Name' -RoomList -PrimarySmtpAddress "Room-Name@windows.onmicrosoft.com"
+New-Distributiongroup -Name <RoomListName> -RoomList -PrimarySmtpAddress <RoomListEmail>
 
 # New Room
-New-Mailbox -Name "New Room Name" -Room
+New-Mailbox -Name <RoomName> -Room
 
 # Add Room to Room List
 Add-DistributionGroupMember -Identity <RoomListName> -Member <RoomMailboxUPN>
